f466c6be983da94c2c9c6d89208583d767272914
onekit-qing <64195283+onekit-qing@users.noreply.github.com>
2020-07-29 18:04:12 +0800

    .

--- toutiao_baidu/client/onekit/ui/view/view.js
+++ toutiao_baidu/client/onekit/ui/view/view.js
@@ -1,9 +1,41 @@
 Component({
- options: {
+    options: {
         addGlobalClass: true,
     },
     properties: {
-        
+        Id: { type: String, value: "" },
+        hoverClass: {
+            type: String,
+            value: ""
+        },
+        hoverStarTime: {
+            type: Number,
+            value: ""
+        },
+        hoverStayTime: {
+            type: Number,
+            value: ""
+        },
+        hoverStopPropagation: {
+            type: Boolean,
+            value: false
+        },
+        disableScroll: {
+            type: Boolean,
+            value: false
+        },
+        hidden: {
+            type: Boolean,
+            value: false
+        },
+        disableScroll: {
+            type: Object,
+            value: {}
+        },
     },
-
+    methods: {
+        view_Tap(e) {
+            this.triggerEvent("Tap", e)
+        }
+    }
 });

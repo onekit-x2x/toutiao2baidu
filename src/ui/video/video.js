3318a649ca53edca83cfff4723872920e636f7ae
lyx0lwz <1850115720@qq.com>
2020-12-04 14:13:12 +0800

    .

--- src/ui/video/video.js
+++ src/ui/video/video.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 import onekit_behavior from '../../behavior/onekit_behavior'
@@ -9,29 +10,18 @@ Component({
   options: {
     addGlobalClass: true,
   },
-  lifetimes: {
-    attached() {
-      if (this.properties.poster === true) {
-        this.setData({poster: 'https://www.baidu.com/img/flexible/logo/pc/result.png'})
-      }
-    }
-  },
   properties: {
     src: {
       type: String,
       value: '',
     },
-    objectFit: {
-      type: String,
-      value: 'contain',
-    },
-    controls: {
-      type: Boolean,
-      value: true,
-    },
     autoplay: {
       type: Boolean,
       value: false,
+    },
+    poster: {
+      type: String,
+      value: ''
     },
     loop: {
       type: Boolean,
@@ -41,20 +31,53 @@ Component({
       type: Boolean,
       value: true,
     },
-
     showPlayBtn: {
       type: Boolean,
       value: true,
     },
-
-    poster: {
-      type: String,
-      value: ''
+    controls: {
+      type: Boolean,
+      value: true,
     },
-    title: {
+    objectFit: {
+      type: String,
+      value: 'contain',
+    },
+    showCenterPlayBtn: {
+      type: Boolean,
+      value: true
+    },
+    playBtnPosition: {
+      type: String,
+      value: 'center',
+      observer(newVal) {
+        if (newVal === 'center') {
+          console.log('1', newVal, this.properties.showPlayBtn, this.properties.showCenterPlayBtn)
+          this.properties.showPlayBtn = false
+          this.properties.showCenterPlayBtn = true
+          console.log('11', newVal, this.properties.showPlayBtn, this.properties.showCenterPlayBtn)
+        }
+        if (newVal === 'bottom') {
+          console.log('2', newVal, this.properties.showPlayBtn, this.properties.showCenterPlayBtn)
+          this.properties.showPlayBtn = true
+          this.properties.showCenterPlayBtn = false
+          console.log('22', newVal, this.properties.showPlayBtn, this.properties.showCenterPlayBtn)
+        }
+        this.setData({
+          showPlayBtn: this.properties.showPlayBtn,
+          showCenterPlayBtn: this.properties.showCenterPlayBtn
+        })
+      }
+    },
+    // 广告做不了
+    preRollUniId: {
       type: String,
       value: '',
     },
+    postRollUniId: {
+      type: String,
+      value: '',
+    }
   },
   methods: {
     video_play(e) {
@@ -66,6 +89,9 @@ Component({
     video_ended(e) {
       this.triggerEvent('ended', e)
     },
+    video_error(e) {
+      this.triggerEvent('error', e)
+    },
     video_timeupdate(e) {
       this.triggerEvent('timeupdate', e)
     },
@@ -75,8 +101,22 @@ Component({
     video_waiting(e) {
       this.triggerEvent('waiting', e)
     },
-    video_error(e) {
-      this.triggerEvent('error', e)
+
+    // 广告做不了
+    trigger_adstart(e) {
+      this.triggerEvent('adstart', e)
+    },
+    trigger_adended(e) {
+      this.triggerEvent('adended', e)
+    },
+    trigger_adload(e) {
+      this.triggerEvent('adload', e)
+    },
+    trigger_adclose(e) {
+      this.triggerEvent('adclose', e)
+    },
+    trigger_aderror(e) {
+      this.triggerEvent('aderror', e)
     },
   }
 })

b951225dcde83faedfec02921947d80440b2e837
lyx0lwz <1850115720@qq.com>
2020-12-16 11:47:14 +0800

    .

---
+++ src/ui/icon/icon/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+
+import onekit_behavior from '../../../behavior/onekit_behavior'
+import toutiao_behavior from '../../../behavior/toutiao_behavior'
+
+Component({
+  behaviors: [onekit_behavior, toutiao_behavior],
+  options: {
+    virtualHost: true
+  },
+  properties: {
+    type: {type: String, value: ''},
+    size: {type: Number, value: 24},
+    color: {type: String, value: ''},
+  }
+})
